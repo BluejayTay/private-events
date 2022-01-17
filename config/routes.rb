fb4b82ee67af7fffd9ebe49bb41fefc19db29b61
BluejayTay <tayrusthoven@gmail.com>
2022-01-17 13:14:19 -0600

    Add devise gem and User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'events/new'
   get 'events/create'
   get 'events/index'
+
+  root to: "event#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
