5774471e7f981980e8938c5b2c41a9c1da4307cb
BluejayTay <tayrusthoven@gmail.com>
2022-01-17 12:53:30 -0600

    add event model

---
+++ db/migrate/20220117172535_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.date :date
+      t.time :time
+      t.string :location
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
